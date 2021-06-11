c00590d488a674f1f135482add659c962915a497
Nicolas Brunaldi de Oliveira <nicolas.brunaldi@gmail.com>
2021-06-11 18:46:29 -0300

    adicionando interfaces

--- src/main/kotlin/Diretor.kt
+++ src/main/kotlin/Diretor.kt
@@ -2,14 +2,14 @@ class Diretor(nome: String,
               cpf: String,
               salario: Double,
               val plr: Double,
-              var senha: String): Funcionario(nome = nome, cpf = cpf, salario = salario) {
+              var senha: String): Funcionario(nome = nome, cpf = cpf, salario = salario), Autenticavel {
 
     override val bonificacao: Double
         get() {
             return 1.1 * salario + this.plr
         }
 
-    fun autentica(senha: String): Boolean {
+    override fun autentica(senha: String): Boolean {
         if (senha.equals(this.senha)){
             return true
         }
--- src/main/kotlin/Gerente.kt
+++ src/main/kotlin/Gerente.kt
@@ -1,14 +1,14 @@
 class Gerente(nome: String,
               cpf: String,
               salario: Double,
-              var senha: String): Funcionario(nome = nome, cpf = cpf, salario = salario) {
+              var senha: String): Funcionario(nome = nome, cpf = cpf, salario = salario), Autenticavel {
 
     override val bonificacao: Double
         get() {
             return salario * 1.1
         }
 
-    fun autentica(senha: String): Boolean {
+    override fun autentica(senha: String): Boolean {
         if (senha.equals(this.senha)){
             return true
         }
